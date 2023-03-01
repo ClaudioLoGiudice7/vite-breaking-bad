2e3e9a5b751c9ab2dc558345158a138ec4f08d1f
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-03-01 15:09:26 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+// IMPORT DEL COMPONENTE
+import MyComponent from "./components/MyComponent.vue";
+import MyOtherComponent from "./components/MyOtherComponent.vue";
+
+// DEFINIZIONE DEL COMPONENTE IN PAGINA
+export default {
+
+  components: {
+    MyComponent, MyOtherComponent
+  },
+};
+</script>
+
+<template>
+  <MyComponent></MyComponent>
+  <MyOtherComponent></MyOtherComponent>
+</template>
+
+
+<style lang="scss"></style>
