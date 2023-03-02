f0b75c9384a3db3d435ea2fd3955f3d69a35538e
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-03-02 14:57:40 +0100

    axios not working

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <script>
 
 // IMPORT DEL COMPONENTE
-import MyComponent from "./components/MyComponent.vue";
-import MyOtherComponent from "./components/MyOtherComponent.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import axios from "axios";
+import { store } from "./assets/store";
 
 // DEFINIZIONE DEL COMPONENTE IN PAGINA
 export default {
 
   components: {
-    MyComponent, MyOtherComponent
+    AppHeader, AppMain,
   },
+
+  return: {
+    store,
+  },
+
+  created() {
+    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0")
+      .then((response) => { store.cards = response; })
+  }
 };
 </script>
 
 <template>
-  <MyComponent></MyComponent>
-  <MyOtherComponent></MyOtherComponent>
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
 </template>
 
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {}
+</script>
+
+<template>
+    <header class="d-flex justify-content align-items-center">
+        <div class="container">
+            <img src="../assets/img/Yu-Gi-Oh!_(Logo).jpg" alt="yu-gi-oh logo">
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    height: 50px;
+    width: 100%;
+}
+</style>
